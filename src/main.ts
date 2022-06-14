d54c42312399d4cd6daab9e5890bccfcab9e7bfb
RomanSavka-Sir <leon.wolflans11@gmail.com>
2022-06-14 14:57:43 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -65,7 +65,7 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(process.env.PORT, process.env.HOST);
+  await app.listen(process.env.PORT || 3000, process.env.HOST || 'localhost');
 }
 
 bootstrap();
