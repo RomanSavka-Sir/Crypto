3ff10170ab9d2929773ea1b348163fac85e83c25
RomanSavka-Sir <leon.wolflans11@gmail.com>
2022-06-14 16:19:52 +0300

    fix

--- src/shared/helpers/twilio.sms.sender.ts
+++ src/shared/helpers/twilio.sms.sender.ts
@@ -1,4 +1,4 @@
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectTwilio, TwilioClient } from 'nestjs-twilio';
 
 require('dotenv').config();
